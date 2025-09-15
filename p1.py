cd3fdb1bf99e103acce670729dd6bfc93d5737ad
Selina9209 <Selinac964@gmail.com>
2025-09-15 10:51:30 -0400

    created a python file to be creepy

---
+++ p1.py
@@ -0,0 +1,9 @@
+# My first program
+# Created on 09/15/2025
+# Purpose: to learn python
+# Editor: Selina B)
+
+name = input("What's your name: ")
+age = input("What's your age: ")
+
+print(f"Hello , {name}, nice knowing you, {age} yo")
